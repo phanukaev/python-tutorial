b561bd6f875fac6dfcabc741795d30dc2bf37de4
Peter Hanukaev <phanukaev@gmail.com>
2024-02-21 13:53:43 -0500

    seesion 1

--- publish-cfg.el
+++
@@ -1,7 +0,0 @@
-(setq org-publish-project-alist
-      '(("py-tutorial"
-         :base-directory "~/repos/python-tutorial/"
-         :publishing-function org-html-publish-to-html
-         :publishing-directory "~/repos/python-tutorial/docs/"
-         :section-numbers t
-	 :html-postamble nil)))
